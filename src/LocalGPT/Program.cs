ef77cc2d2f9278ed3e198063f14220ad217ce85d
LEGENDARYSONIC\micha <michael.f.1991@outlook.com>
2026-08-09 13:05:18 +0200

    test

--- src/LocalGPT/Program.cs
+++ src/LocalGPT/Program.cs
@@ -261,7 +261,7 @@ namespace LocalGPT
 
                 // PublisherStudio-style application boundaries: runtime helpers are injected services,
                 // not mutable process-wide utility classes.
-                builder.Services.AddSingleton<ICustomVersion>(new CustomVersion("2.4.8"));
+                builder.Services.AddSingleton<ICustomVersion>(new CustomVersion("2.4.9"));
                 builder.Services.AddSingleton<LocalGptCatalogService>();
                 builder.Services.AddSingleton<ILocalGptRequestFactoryService, LocalGptRequestFactoryService>();
                 builder.Services.AddSingleton<ICouncilTextPatternDataService, CouncilTextPatternDataService>();
